b365b86e4ffee9bacad480765d4efbea41699656
mori0 <mori007v@gmail.com>
2023-10-21 17:14:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,38 @@
-import requests, os, colorama
-from colorama import Fore , Back , Style
-colorama.init (autoreset = True)
+import requests
+import os
+import colorama
+import time
+from colorama import Fore, Back, Style
 
-os.system('pip install requests')
-os.system('pip install colorama')
-
-print(f"""{Fore.TEAL}
-                  _ _                _               _             
-__   ____ _ _ __ (_) |_ _   _    ___| |__   ___  ___| | _____ _ __ 
-\ \ / / _` | '_ \| | __| | | |  / __| '_ \ / _ \/ __| |/ / _ \ '__|
- \ V / (_| | | | | | |_| |_| | | (__| | | |  __/ (__|   <  __/ |   
-  \_/ \__,_|_| |_|_|\__|\__, |  \___|_| |_|\___|\___|_|\_\___|_|   
-                        |___/       
--------------------------------------------------------------------
-"""
-
+colorama.init(autoreset=True)
 
 valid = 0
 invalid = 0
-with open('list.txt','r') as handle:
-        list = handle.readlines()
-        for van in list:
-            vanity = van.rstrip()
-            check = requests.get(f"https://discord.com/api/v9/invites/{vanity}")
-            if check.status_code ==404:
-             print(f"{Fore.GREEN} {vanity} Vaild")
-             with open("vaild.txt","a+") as file:
-              file.write(vanity)
-              file.write("\n")
-              Vaild =+ 1
-              
-            else:
-             print(f"{Fore.RED} {vanity} invaild")
-              invaild =+ 1
-        
-        print(f"went through list.. {valid} valid, {invalid} invalid, total {len(list)}")
+words_left_out = 0
+word_set = set()
+
+with open('list.txt', 'r') as handle:
+    lines = handle.readlines()
+    total_words = len(lines)
+    for van in lines:
+        vanity = van.rstrip()
+        if len(vanity) < 3:
+            words_left_out += 1
+            continue
+        word_set.add(vanity)
+    
+    for vanity in word_set:
+        check = requests.get(f"https://discord.com/api/v10/invites/{vanity}")
+        if check.status_code == 404:
+            print(f"{Fore.GREEN}{vanity} Valid")
+            with open("valid.txt", "a+") as file:
+                file.write(vanity)
+                file.write("\n")
+            valid += 1
+        else:
+            print(f"{Fore.RED}{vanity} Invalid")
+            invalid += 1
+        time.sleep(0.7)
+
+print(f"{words_left_out} words were left out as they were less than 3 letters.")
+print(f"Went through the list... {valid} valid, {invalid} invalid, total {total_words}")
